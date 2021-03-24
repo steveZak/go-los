8b55bb29c09305eca5bb5af258134a349ad5d2b8
Steve <chesl97@gmail.com>
2021-03-24 17:53:59 -0400

    my video data processing done

--- py-los/train.py
+++ py-los/train.py
@@ -1,10 +1,10 @@
 import json
 import os
 import cv2
-import imageio
+import random
 import numpy as np
 import tensorflow as tf
-from tensorflow.python.keras import layers, losses, models, optimizers
+from tensorflow.keras import layers, losses, models, optimizers
 from ctypes import *
 
 pigo = cdll.LoadLibrary('./talkdet.so')
@@ -52,28 +52,45 @@ def process_frame(pixs):
         dets = list(res.reshape(-1, ARRAY_DIM))[0:dets_len*19]
         return dets
 
-def getImages(reader, start, end, frame_rate=30):
+def getImages(frame_bounds, frame_rate=30):
     frames = []
-    print(round(30*start))
-    print(round(30*end))
-    for i in range(round(30*start), round(30*end)):
-        image = reader.get_data(i)
-        # image = reader[i]
-        # break
-        b,g,r = cv2.split(image)           # get b, g, r
-        image = cv2.resize(cv2.merge([r,g,b])[190:1100, 0:1400, :], (640, 480))
-        pixs = np.ascontiguousarray(image[:,:, 1].reshape((image.shape[0], image.shape[1])))
-        pixs1 = pixs.flatten()
-        dets = process_frame(pixs1)
-        if len(dets)> 0:
-            mouth = cv2.resize(image[dets[16][0]-5:dets[15][0]+5, dets[14][1]-5:dets[17][1]+5], (200, 100))
-            # cv2.imwrite('cool1.png', mouth)
-            frames.append(mouth)
+    filename = 'gatsby2.MOV'
+    # reader = imageio.get_reader(filename,  'ffmpeg')
+    cap = cv2.VideoCapture(filename)
+    j = 0
+    print(frame_bounds)
+    for i in range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))):
+        # print(j)
+        res, image = cap.read()
+        if i >= round(cv2.CAP_PROP_FPS*frame_bounds[j][0]) and i <= round(cv2.CAP_PROP_FPS*frame_bounds[j][1]):
+            # image = reader.get_data(i)
+            # res, image = cap.retrieve(flag=7)
+            # image = reader[i]
+            # break
+            # b,g,r = cv2.split(image)           # get b, g, r
+            # image = cv2.resize(cv2.merge([r,g,b])[190:1100, 0:1400, :], (640, 480))
+            image = cv2.resize(image[190:1100, 0:1400, :], (640, 480))
+            pixs = np.ascontiguousarray(image[:,:, 1].reshape((image.shape[0], image.shape[1])))
+            pixs1 = pixs.flatten()
+            dets = process_frame(pixs1)
+            if len(dets)> 0:
+                mouth = cv2.resize(image[dets[16][0]-5:dets[15][0]+5, dets[14][1]-5:dets[17][1]+5], (200, 100))
+                cv2.imwrite('cool2.png', mouth)
+                frames.append(mouth)
+            print("time="+str(i/cv2.CAP_PROP_FPS))
+        if i >= round(cv2.CAP_PROP_FPS*frame_bounds[j][1]):
+            j+=1
+            if j==len(frame_bounds):
+                return frames
+        if i==6400:
+            return frames
+            # print("time="+str(frame_bounds[j][1]))
     return frames
 
 def getReader():
     filename = 'gatsby2.MOV'
-    reader = imageio.get_reader(filename,  'ffmpeg')
+    # reader = imageio.get_reader(filename,  'ffmpeg')
+    reader = cv2.VideoCapture(filename)
     return reader
 
 def oneHot(idx, length):
@@ -81,45 +98,41 @@ def oneHot(idx, length):
     onehot[0, idx] = 1
     return onehot
 
-def getDataset(reader, phonemes):
+def getDataset(phonemes):
     X, Y = [], []
     idx = 0
-    # print(len(phonemes[phoneme]))
     for phoneme in phonemes:
+        print(phoneme)
+        print(len(phonemes[phoneme]))
         # print(phoneme)
-        images = np.array([], dtype=np.int64).reshape(0, 100, 200, 3)
-        labels = np.array([], dtype=np.int64).reshape(0, len(phonemes.keys()))
-        i=0
-        for frames in phonemes[phoneme]:
-            i+=1
-            _images = getImages(reader, frames[0], frames[1])
-            for image in _images:
-                images = np.vstack([images, image[np.newaxis, ...]])
-                labels = np.vstack([labels, oneHot(idx, len(phonemes))])
+        images = np.array([], dtype=np.float32).reshape(0, 100, 200, 3)
+        labels = np.array([], dtype=np.float32).reshape(0, len(phonemes.keys()))
+        _images = getImages(phonemes[phoneme])
+        for image in _images:
+            images = np.vstack([images, image[np.newaxis, ...]])
+            labels = np.vstack([labels, oneHot(idx, len(phonemes))])
         X.append(images)
         Y.append(labels)
-        print(len(X))
-        print(len(Y))
         idx+=1
-        if idx>1:
-            break
+        # if idx>1:
+        #     break
     return X, Y
 
 # class CNN():
 #     def __init__():
         
 
-def createNN():
+def createNN(bs=1):
     model = models.Sequential()
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(640, 480, 3)))
+    model.add(layers.Conv2D(bs, (3, 3), activation='relu', input_shape=(100, 200, 3)))
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(640, 480, 3)))
+    model.add(layers.Conv2D(bs, (3, 3), activation='relu', input_shape=(100, 200, 3)))
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(640, 480, 3)))
+    model.add(layers.Conv2D(bs, (3, 3), activation='relu', input_shape=(100, 200, 3)))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Flatten())
     model.add(layers.Dense(300))
-    model.add(layers.Dense(39))
+    model.add(layers.Dense(40))
     return model
 
 def forward(model, X):
@@ -128,8 +141,9 @@ def forward(model, X):
 def backprop(model, X, Y):
     Y_hat = forward(model, X)
     loss = losses.BinaryCrossentropy()
-    opt = optimizers.SGD()
+    opt = optimizers.Adam()
     opt.minimize(loss)
+    return
     #backprop the model
 
 
@@ -148,24 +162,45 @@ def readPhonemes(fname):
                 phoneme_start += phone["duration"]
     # remove out of vocabulary and silent phonemes
     phonemes.pop("oov", None)
-    phonemes.pop("sil", None)
+    # phonemes.pop("sil", None)
     # combine similar phonemes
     # phonemes[].extend(phonemes[])
     # phonemes.pop()
     return phonemes
 
+def getShuffledIndices(y):
+    indices = []
+    for i in range(len(y)):
+        for j in range(len(y[i])):
+            indices.append([i, j])
+    random.shuffle(indices)
+    return indices
+
 
 # phonemes = readPhonemes("gatsby2resfinal.json")
-# reader = getReader()
-# X, Y = getDataset(reader, phonemes)
+# print(phonemes.keys())
+# ['ah', 'b', 'aw', 't', 'hh', 'ae', 'f', 'w', 'ey', 'ih', 'iy', 'n', 'eh', 's', 'g', 'd', 'uw', 'y', 'ao', 'r', 'k', 'dh', 'm', 'ow', 'er', 'l', 'jh', 'oy', 'z', 'ay', 'v', 'sh', 'ng', 'aa', 'ch', 'th', 'p', 'zh', 'uh', 'sil']
+# # reader = getReader()
+# X, Y = getDataset(phonemes)
 # with open('X.npy', 'wb') as f:
-#     np.save(f, X)
+#     np.save(f, X, allow_pickle=True)
 # with open('Y.npy', 'wb') as f:
-#     np.save(f, Y)
+#     np.save(f, Y, allow_pickle=True)
 # print(len(Y))
 # print(Y[0])
 x = np.load("X.npy", allow_pickle=True)
-print(x)
+print(x[1].shape)
 y = np.load("Y.npy", allow_pickle=True)
-print(y)
+print(y[1].shape)
 model = createNN()
+print(model)
+out = forward(model, x[0][1].astype(np.float32)[np.newaxis, ...])
+indices = getShuffledIndices(y)
+print(indices[0:5])
+e = 0
+while e<20:
+    for i in range(len(indices)):
+        backprop(model, x[indices[i]][0][indices[i]][1], y[indices[i]][0][indices[i]][1])
+    e += 1
+# out = forward(model, x[0][0:5].astype(np.float32))
+print(out)
